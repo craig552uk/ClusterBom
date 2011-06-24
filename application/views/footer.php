e6b6063aba658ee0b33ecc4b230a8568e9f7f650
Craig Russell <craig@craig-russell.co.uk>
2011-06-24 11:57:25 +0100

    Added parts from HTML5 Boilerplate project

--- application/views/footer.php
+++ application/views/footer.php
@@ -8,5 +8,19 @@
  * @package PIP
  */
 ?>
+    <script src="<?php echo BASE_URL; ?>static/js/libs/jquery-1.5.1.min.js"></script>
+
+    <!--[if lt IE 7 ]>
+    <script src="<?php echo BASE_URL; ?>static/js/libs/dd_belatedpng.js"></script>
+    <script>DD_belatedPNG.fix("img, .png_bg");</script>
+    <![endif]-->
+    
+    <script>
+        var _gaq=[["_setAccount","UA-XXXXX-X"],["_trackPageview"]];
+        (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
+        g.src=("https:"==location.protocol?"//ssl":"//www")+".google-analytics.com/ga.js";
+        s.parentNode.insertBefore(g,s)}(document,"script"));
+    </script>
+    
 </body>
 </html>
--- application/views/header.php
+++ application/views/header.php
@@ -9,14 +9,26 @@
  */
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<!--[if lt IE 7 ]> <html class="no-js ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]>    <html class="no-js ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]>    <html class="no-js ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <head>
     <meta charset="utf-8" />
     
     <title><?php echo ( isset($title)) ? $title : ''; ?></title>
     <meta name="description" content="">
     <meta name="author" content="">
+    <link rel="author" href="<?php echo BASE_URL; ?>humans.txt" />
     
-    <link rel="stylesheet" href="<?php echo BASE_URL; ?>static/css/style.css" type="text/css" media="screen" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="<?php echo BASE_URL; ?>static/images/icons/favicon.ico">
+    <link rel="apple-touch-icon" href="<?php echo BASE_URL; ?>static/images/icons/apple-touch-icon.png">
+    
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>static/css/boilerplate.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>static/css/style.css" type="text/css" />
+    
+    <script src="<?php echo BASE_URL; ?>static/js/libs/modernizr-1.7.min.js"></script>
 </head>
 <body>
