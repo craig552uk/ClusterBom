a4b5de98eabf88be095def4d833f72b6de44c65c
Craig Russell <craig@craig-russell.co.uk>
2011-06-24 12:51:58 +0100

    Refined debug display

--- application/views/debug.php
+++ application/views/debug.php
@@ -1,28 +1,35 @@
 <?php if(DEBUG): ?>
 <section id="debug">
     <fieldset>
-    <legend>Session Data</legend>
-    <pre><?php print_r($_SESSION); ?></pre>
-    </fieldset>
+    <legend>Debug Data</legend>
+        
+        <p>Data displayed in this section can be edited in the debug.php view 
+           file. Debugging is enabled by a global variable set in the index.php
+           file in the root directory.</p>
     
-    <fieldset>
-    <legend>GET Data</legend>
-    <pre><?php print_r($_GET); ?></pre>
-    </fieldset>
+        <?php
+            debug_block('Session Data', $_SESSION);
+            debug_block('GET Data', $_GET);
+            debug_block('POST Data', $_POST);
+            debug_block('Cookie Data', $_COOKIE);
+            debug_block('Server Data', $_SERVER);
+        ?>
     
-    <fieldset>
-    <legend>POST Data</legend>
-    <pre><?php print_r($_POST); ?></pre>
-    </fieldset>
-    
-    <fieldset>
-    <legend>Cookie Data</legend>
-    <pre><?php print_r($_COOKIE); ?></pre>
-    </fieldset>
-    
-    <fieldset>
-    <legend>Server Data</legend>
-    <pre><?php print_r($_SERVER); ?></pre>
     </fieldset>
 </section>
 <?php endif; ?>
+<?php
+
+    /*
+     * Displays the contents of a data array in an HMTL fieldset
+     *
+     * @param string $legend The legend of the field set
+     * @param mixed  $data   The data to be displayed
+     */
+    function debug_block($legend, $data){
+        echo "<fieldset><legend>$legend</legend><pre>";
+        print_r($data);
+        echo "</pre></fieldset>";
+    }
+
+?>
