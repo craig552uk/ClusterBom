a78a62445bcdfe536d30daf4ccebe230f6976f19
Craig Russell <craig@craig-russell.co.uk>
2011-06-27 12:14:24 +0100

    Added user table queries

--- docs/sql/CommonQueries.sql
+++ docs/sql/CommonQueries.sql
@@ -4,3 +4,7 @@ INSERT INTO data_file (name, hash, path, size, description) VALUES(?, ?, ?, ?, ?
 UPDATE data_file (fk_cust_id, name, hash, path, size, description, date_last_accessed, public) VALUES(?, ?, ?, ?, ?, ?, ?, ?) WHERE pk_file_id=?;
 
 UPDATE data_file SET fk_cust_id=?, name=?, hash=?, path=?, size=?, description=?, date_last_accessed=?, public=? WHERE pk_file_id=?
+
+INSERT INTO user SET pk_cust_id='', goog_id='', fname='', sname='', email='', administrator='', account_enabled='', login_count='', fk_plan_id='', credit='', unpaid_alert='';
+
+INSERT INTO user SET goog_id='my_google_id', fname='Test', sname='User', email='test@test.com', fk_plan_id='FREE';
