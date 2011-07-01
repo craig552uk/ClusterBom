f4f1c25057f2dd324ec207f88d70046d4d1475fc
Craig Russell <craig@craig-russell.co.uk>
2011-07-01 15:48:08 +0100

    Edit account settings and password

--- application/controllers/account.php
+++ application/controllers/account.php
@@ -24,19 +24,98 @@ class Account extends Controller {
         // Secure access only
         if(!$this->session->isAuth()) { header('Location: '.BASE_URL); }
         
-        // Get user data
+        if(isset($_POST['submit'])){
+            $this->update_account();
+        }else{
+            // Get user data
+            $user = $this->load->model('User');
+            $user->loadByEmail( $this->session->email );
+            
+            // Load view
+            $template = $this->load->view('app/user-profile');
+            $template->set('title','Account');
+            $template->set('message', "Account Profile");
+            $template->set('session', $this->session);
+            $template->set('account', $user);
+            $template->render();
+        }
+	}
+
+	/**
+	 * Save changes to account
+	 */
+	private function update_account(){
+	    // Get data from post
+	    $name     = (isset($_POST['name']))     ? $_POST['name'] : '';
+        $email    = (isset($_POST['email']))    ? $_POST['email'] : '';
+        $old_pass = (isset($_POST['old_pass'])) ? $_POST['old_pass'] : '';
+        $new_pass = (isset($_POST['new_pass'])) ? $_POST['new_pass'] : '';
+        
+        // Create user object
         $user = $this->load->model('User');
         $user->loadByEmail( $this->session->email );
         
-        // Load view
-        $template = $this->load->view('app/user-profile');
-        $template->set('title','Account');
-        $template->set('message', "Account Profile");
-        $template->set('session', $this->session);
-        $template->set('account', $user);
-        $template->render();
+        // Prepare view
+        $view = $this->load->view('app/user-profile');
+        
+        /* Account Update */
+        if(($name != $user->name) || ($email != $user->email)){
+            // Sanity check
+            if(($name=='') || ($email=='')){
+                $details_error = 'Name and email cannot be empty';
+            }
+            // Email validity TODO better email check
+            elseif(strpos($email, '@') === false){
+                $details_error = 'Email must be valid';
+            }
+            
+            // Handle errors
+            if(isset($details_error)){
+                $view->set('details_error', $details_error);
+            }else{
+                $view->set('details_success', 'Changes successfully saved');
+                // Save changes
+                $user->name = $name;
+                $user->email = $email;
+                $user->save();
+            }
+        }
+        
+        /* Password Change */
+        if(($old_pass=='') && ($new_pass=='')){
+            // do nowt
+        }else{
+            // Password changing
+        
+            // Error password failure
+            $old_pass = sha1('RdGBEe8scaJx'.$user->id.$old_pass);
+            if($user->password != $old_pass ){
+                $password_error = 'Current password incorrect';
+            }
+            // Password Length TODO use decent policy
+            elseif(strlen($new_pass) < 8){
+                $password_error = 'New password must be at least 8 characters';
+            }
+            
+            // Handle errors
+            if(isset($password_error)){
+                $view->set('password_error', $password_error);
+            }else{
+                $view->set('password_success', 'Password successfully changed');
+                $user->password = sha1('RdGBEe8scaJx'.$user->id.$new_password);
+                $user->save();
+            }
+        }
+        
+        // Render view
+        $view->set('title','Account');
+        $view->set('message', "Account Profile");
+        $view->set('session', $this->session);
+        $view->set('account', $user);
+        $view->render();
+        
 	}
-	
+		
 	/**
      * Account payment plan
      */
@@ -68,8 +147,6 @@ class Account extends Controller {
         $template->set('session', $this->session);
         $template->render();
 	}
-	
-	
     
 }
 
--- application/views/app/user-profile.php
+++ application/views/app/user-profile.php
@@ -6,13 +6,41 @@
 ?>
 <?php include(APP_DIR.'views/sys/header.php'); ?>
 
-<div class="app">
+<div class="app settings">
     <section>
         <h1>My Account</h1>
-        <dl>
-            <dt>Name</dt><dd><?php echo $account->fname . ' ' . $account->sname; ?></dd>
-            <dt>Email</dt><dd><?php echo $account->email; ?></dd>
-        </dl>
+        <form method="post" action="">
+            <?php if(isset($details_success)): ?>
+                <p class="success"><?php echo $details_success; ?></p>
+            <?php endif; ?>
+            <?php if(isset($password_success)): ?>
+                <p class="success"><?php echo $password_success; ?></p>
+            <?php endif; ?>
+            <fieldset>
+                <legend>Account Details</legend>
+                <?php if(isset($details_error)): ?>
+                    <p class="error"><?php echo $details_error; ?></p>
+                <?php endif; ?>
+                <p><label for="name">Name</label>
+                   <input type="text" name="name" id="name" value="<?php echo $account->name; ?>" /></p>
+                <p><label for="email">Email</label>
+                   <input type="text" name="email" id="email" value="<?php echo $account->email; ?>" /></p>
+            </fieldset>
+            
+            <fieldset>
+                <legend>Change Password</legend>
+                <?php if(isset($password_error)): ?>
+                    <p class="error"><?php echo $password_error; ?></p>
+                <?php endif; ?>
+                <p><label for="old_pass">Old Password</label>
+                   <input type="password" name="old_pass" id="old_pass" /></p>
+                <p><label for="new_pass">New Password</label>
+                   <input type="password" name="new_pass" id="new_pass" /></p>
+            </fieldset>
+            
+            <div><input type="submit" name="submit" id="submit" value="Save" />
+               <a class="cancel" href="<?php echo BASE_URL; ?>">Cancel</a></div>
+        </form> 
     </section>
 </div><!-- .content -->
 
