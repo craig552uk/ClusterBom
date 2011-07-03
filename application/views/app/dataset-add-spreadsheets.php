d860640658d91595cdc5f721caaab4ab02352f85
Craig Russell <craig@craig-russell.co.uk>
2011-07-03 13:09:39 +0100

    Styles for drop-down worksheets

--- application/views/app/dataset-add-spreadsheets.php
+++ application/views/app/dataset-add-spreadsheets.php
@@ -7,10 +7,17 @@
 
 <ul>
 <?php foreach($spreadsheets as $s): ?>
-    <li class="clearfix">
-        <div class="title"><?php echo $s->title; ?></div> 
-        <div class="author"><?php echo $s->author->name; ?></div>
-        <div class="date"><?php echo date(DATETIME_FORMAT, $s->updated); ?></div>
+    <li>
+        <div class="spreadsheet clearfix" onclick="$(this).nextAll().slideToggle();">
+            <span class="title"><?php echo $s->title; ?></span> 
+            <span class="author"><?php echo $s->author->name; ?></span>
+            <span class="date"><?php echo date(DATETIME_FORMAT, $s->updated); ?></span>
+        </div>
+        <ul class="worksheets">
+            <li class="worksheet">Worksheet1</li>
+            <li class="worksheet">Worksheet2</li>
+            <li class="worksheet">Worksheet3</li>
+        </ul>
     </li>
     <!--<p><?php echo $s->uri; ?></p>
     <p><?php echo $s->updated; ?></p>
