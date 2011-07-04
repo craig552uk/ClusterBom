171d47295b51d36bae8905bdc8844e302e279a36
Craig Russell <craig@craig-russell.co.uk>
2011-07-04 16:43:31 +0100

    Null table headers in step 3

--- application/views/app/dataset-add-cells.php
+++ application/views/app/dataset-add-cells.php
@@ -4,24 +4,31 @@
  */
 ?>
 
-<div class="cells hscroll frame">
+<div class="cells scroll frame">
     <table width="100%">
-    
-        <?php for($r=1; $r<$max_row; $r++): ?>
-            <tr id="row-<?php echo $r; ?>">
-            
+        <thead>
+            <tr>
                 <?php for($c='A'; $c<$max_col; $c=$parent->nextCol($c)): ?>
-                    <?php $v = (isset($cells[$c][$r])) ? $cells[$c][$r] : '&nbsp;'; ?>
-                    <td id="cell-<?php echo $c.$r; ?>"><?php echo $v; ?></td>
+                    <th id="head-<?php echo $c; ?>"><?php echo 'Nowt'; ?></th>
                 <?php endfor;?>
-                
             </tr>
-        <?php endfor;?>
-        
+        </thead>
+        <tbody>
+            <?php for($r=1; $r<$max_row; $r++): ?>
+                <tr id="row-<?php echo $r; ?>" class="dr">
+                
+                    <?php for($c='A'; $c<$max_col; $c=$parent->nextCol($c)): ?>
+                        <?php $v = (isset($cells[$c][$r])) ? $cells[$c][$r] : '&nbsp;'; ?>
+                        <td id="cell-<?php echo $c.$r; ?>"><?php echo $v; ?></td>
+                    <?php endfor;?>
+                    
+                </tr>
+            <?php endfor;?>
+        </tbody>
     </table>
 </div>
 <script type="text/javascript">
-    $('tr').click(function(){
+    $('.dr').click(function(){
         $(this).toggleClass('ex');
         var x='';
         $('.ex').each(function(){
